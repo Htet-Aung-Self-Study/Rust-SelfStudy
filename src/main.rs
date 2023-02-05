dfa25ed4f87eeef2734acc6f0b5a2ca1ac9e8406
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-02-05 17:53:06 +0800

    get-serial-number Commit #1
    trying to get serial number from WMIC command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{process::Command};
+
 fn main() {
-    println!("Hello, world!");
+
+    //get the command line result
+    let output_result  = Command::new("wmic")
+                                .arg("bios")
+                                .arg("get")
+                                .arg("serialnumber")
+                                .output().expect("Something happened!?");
+    let commandline_output_result = String::from_utf8_lossy(&output_result.stdout).to_string();
+    
+    //and read the line 1
+    let mut count = 0;
+    for line in commandline_output_result.lines() {
+        if count == 1 {
+            println!("{}", line);
+        }
+        count += 1;
+    }
 }
