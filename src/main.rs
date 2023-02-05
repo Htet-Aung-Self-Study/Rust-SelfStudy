f2b34bc13b63c0acefe005dcb34eec7a86a38002
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-02-05 21:24:57 +0800

    Set Registry in Current Users
    It is working now!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+extern crate winreg;
+
 use std::{process::Command};
+use std::path::Path;
+
+use winreg::enums::*;
+use winreg::RegKey;
+
+const company: &str = "Company";
 
 fn main() {
 
@@ -11,11 +19,18 @@ fn main() {
     let commandline_output_result = String::from_utf8_lossy(&output_result.stdout).to_string();
     
     //and read the line 1
+    let mut serialnumber: String = "".into();
     let mut count = 0;
     for line in commandline_output_result.lines() {
         if count == 1 {
+            serialnumber = line.into();
             println!("{}", line);
         }
         count += 1;
     }
+
+    let hklm = RegKey::predef(HKEY_CURRENT_USER);
+    let path = Path::new("Software").join(company);
+    let (key, _disp) = hklm.create_subkey(&path).unwrap();
+    key.set_value("ID", &serialnumber);
 }
